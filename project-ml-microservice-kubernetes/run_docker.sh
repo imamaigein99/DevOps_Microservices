96cc0cdd566988fb3d82215712b075c4484687ce
imamaigein99 <iimama@yahoo.com>
2022-07-30 16:52:38 +0100

    Imama Igein First submission for final project

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -3,10 +3,12 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Build image and add a descriptive tag
+docker build --tag=udacitypredictor . 
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+docker run -p 8080:80 udacitypredictor
--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="imama42/udacitypredictor"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacitypredictorpod --image=$dockerpath --port=80 --labels app=udacitypredictorpod
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods -o wide 
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacitypredictorpod 8080:80
